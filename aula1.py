d2fb4555942694837f087332ca43d89d7a8d2d32
deyvsonfl <deyvsonflp99@gmail.com>
2025-02-07 23:21:05 -0300

    Criação e configuração dos frames

--- aula1.py
+++ aula1.py
@@ -5,13 +5,22 @@ window = Tk()
 class Application():
   def __init__(self):
     self.window = window
-    self.tela()
+    self.display()
+    self.display_frames()
     window.mainloop()
-  def tela(self):
+  def display(self):
     self.window.title("Cadastro de Clientes")
     self.window.configure(background= '#fdfdfd')
     self.window.wm_maxsize(1280, 1080)
     self.window.wm_minsize(788, 588)
     self.window.resizable(True, True)
+  def display_frames(self):
+    self.frame_1 = Frame(self.window, bd=4, bg='#dfe3ee', highlightbackground='#759fe6', highlightthickness=3)
+    self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.46)
+
+    self.frame_2 = Frame(self.window, bd=4, bg='#dfe3ee', highlightbackground='#759fe6', highlightthickness=3)
+    self.frame_2.place(relx=0.02, rely=0.5, relwidth=0.96, relheight=0.46)
+
+  
 
 Application()
